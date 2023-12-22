6f64b68b97002ee784fdd9e25ac3958270a5c2eb
Berkay Urun <berkayurun@outlook.com>
2023-12-22 23:57:38 +0300

    refactor: complete 1.5 object refactor

--- part1/course_information/src/App.tsx
+++ part1/course_information/src/App.tsx
@@ -35,27 +35,29 @@ const Total = ({ exerciseCounts }: { exerciseCounts: number[] }) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <div>
-      <Header header={course} />
-      <Content partsToRender={[part1, part2, part3]} />
-      <Total
-        exerciseCounts={[part1.exercises, part2.exercises, part3.exercises]}
-      />
+      <Header header={course.name} />
+      <Content partsToRender={course.parts} />
+      <Total exerciseCounts={course.parts.map((part) => part.exercises)} />
     </div>
   );
 };
