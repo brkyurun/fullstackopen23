9ddff57dc78baa970fd32c17fa5d75f57ed91ee6
Berkay Urun <berkayurun@outlook.com>
2023-12-24 21:12:18 +0300

    feat: complete challenge 2.9

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -2,10 +2,19 @@ import { ChangeEvent, FormEvent, useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '040-1234567' },
+    { name: 'Arto Hellas', phoneNumber: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phoneNumber: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122', id: 4 },
   ]);
   const [newName, setNewName] = useState('');
   const [newPhoneNumber, setNewPhoneNumber] = useState('');
+  const [filterQuery, setFilterQuery] = useState('');
+  const peopleToRender = filterQuery
+    ? persons.filter((person) =>
+        person.name.toLowerCase().startsWith(filterQuery)
+      )
+    : persons;
 
   const handleFormSubmit = (ev: FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
@@ -15,7 +24,13 @@ const App = () => {
       return;
     }
 
-    setPersons(persons.concat({ name: newName, phoneNumber: newPhoneNumber }));
+    setPersons(
+      persons.concat({
+        name: newName,
+        phoneNumber: newPhoneNumber,
+        id: persons.length + 1,
+      })
+    );
     setNewName('');
     setNewPhoneNumber('');
   };
@@ -28,9 +43,22 @@ const App = () => {
     setNewPhoneNumber(ev.target.value);
   };
 
+  const handleFilterQueryInput = (ev: ChangeEvent<HTMLInputElement>) => {
+    setFilterQuery(ev.target.value);
+  };
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with:{' '}
+        <input
+          type="text"
+          value={filterQuery}
+          onChange={handleFilterQueryInput}
+        />
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input type="text" onChange={handleNameInput} value={newName} />
@@ -48,7 +76,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {peopleToRender.map((person) => (
         <p key={person.name}>
           {person.name} {person.phoneNumber}
         </p>
