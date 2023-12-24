dc342de37e8a1029bf7dbf386b4f747712a5de9c
Berkay Urun <berkayurun@outlook.com>
2023-12-24 18:14:07 +0300

    feat: complete challenge 1.8

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,5 +1,35 @@
 import { useState } from 'react';
 
+type StatisticsProps = {
+  good: number;
+  neutral: number;
+  bad: number;
+  allVotes: number;
+  average: number;
+  positiveRatio: number;
+};
+
+const Statistics = ({
+  good,
+  neutral,
+  bad,
+  allVotes,
+  average,
+  positiveRatio,
+}: StatisticsProps) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {allVotes}</p>
+      <p>average {isNaN(average) ? 0 : average}</p>
+      <p>positive {isNaN(positiveRatio) ? 0 : positiveRatio} %</p>
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -21,13 +51,14 @@ const App = () => {
       <button type="button" onClick={() => setBad(bad + 1)}>
         bad
       </button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {allVotes}</p>
-      <p>average {isNaN(average) ? 0 : average}</p>
-      <p>positive {isNaN(positiveRatio) ? 0 : positiveRatio} %</p>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        allVotes={allVotes}
+        average={average}
+        positiveRatio={positiveRatio}
+      />
     </div>
   );
 };
