abeed8b2aa3b379d5878b91379d101df6cf1f4b5
Berkay Urun <berkayurun@outlook.com>
2023-12-24 20:59:09 +0300

    feat: complete exercise 2.6

---
+++ part2/phonebook/src/App.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleFormSubmit = (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  };
+
+  const handleInput = (ev: ChangeEvent<HTMLInputElement>) => {
+    setNewName(ev.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          name: <input type="text" onChange={handleInput} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
